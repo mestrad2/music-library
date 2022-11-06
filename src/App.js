ffb7819155f025405f660f4547cc077326a258c2
Manuel Estrada <manuel.estrada16@gmail.com>
2022-11-05 23:12:14 -0400

    added suspense functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { createResource as fetchData } from './helper'
+
 
 function App() {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
-  let [data, setData] = useState([])
+  let [data, setData] = useState(null)
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
   useEffect(() => {
     if (search) {
-      const fetchData = async () => {
-        document.title = `${search} music`
-        const response = await fetch(API_URL + search)
-        const resData = await response.json()
-        if (resData.results.length > 0) {
-          return setData(resData.results)
-        } else {
-          return setMessage('Not Found.')
-        }
-      }
-      fetchData()
+      setData(fetchData(search))
     }
   }, [search])
 
+
+  const renderGallery = () => {
+    if (data) {
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      )
+    }
+  }
 
   const handleSearch = (e, term) => {
     e.preventDefault()
@@ -32,13 +34,12 @@ function App() {
   }
 
   return (
-    <div>
+    <div className="App">
       <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      {renderGallery()}
     </div>
   )
-
 }
 
 export default App
